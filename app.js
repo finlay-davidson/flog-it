8ea962a0f9ea373a8a9a830cb58320de2d4177c5
Finlay Davidson <finlay.w.davidson@gmail.com>
2025-12-15 09:28:02 +1100

    fix

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import express from "express";
-//import listingsRouter from "./routes/listings.js";
+import listingsRouter from "./routes/listings.js";
 
 const app = express();
 app.use(express.json());
 
-//app.use("/listings", listingsRouter);
+app.use("/listings", listingsRouter);
 
 app.get("/", (req, res) => res.send("Marketplace API running"));
 
